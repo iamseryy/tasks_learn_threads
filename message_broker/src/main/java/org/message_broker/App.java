16358979acb1765a2ab28a5f77f635cb2f87605b
permserg <permserg@gmail.com>
2023-12-24 13:57:08 +0300

    message_broker init

---
+++ message_broker/src/main/java/org/message_broker/App.java
@@ -0,0 +1,13 @@
+package org.message_broker;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
